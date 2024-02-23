8d7b798d6a05f5bfb925b53215570e9232b8c925
ickembooi <kevinkipkemboi3@gmail.com>
2024-02-23 12:45:34 +0300

    initial commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input("What's your name?: ")
+age= input("What's your age?: ")
+location=input("What's your location?: ")
+print("Hello "+ name, " you are "+age, " years old and live in "+location)
